667b2902d44b131fe408da210da9f96cb522981f
CHWu <wuchin1997@gmail.com>
2021-10-12 13:05:17 +0200

    init main

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+bool gameAction();//return value "false" only when save
+
+int main(int argc, char *argv[])){
+
+    if(false){     //check if user have save file
+        if(!chose){ // chose = 0 , overrider save
+
+        }else{ // chose = 1 , load current profile
+
+        }
+
+    }
+
+    //generation map
+
+    //other action
+
+    // run game
+    bool gameProgress ;
+    gameProgress = true;
+    while(gameProgress){
+        gameProgress = gameAction();
+    }
+
+
+
+}
