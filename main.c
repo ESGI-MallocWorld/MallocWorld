da32c610c618822c7a6cc1d25a412ea5e66c0f7e
CHWu <wuchin1997@gmail.com>
2021-11-12 18:35:53 +0100

    add test condition in main file

--- main.c
+++ main.c
@@ -1,36 +1,47 @@
-#include <stdbool.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 #include "./functions/mapAction.h"
 
+void echo(char *string){
+    printf("%s \n",string);
+}
 
-bool gameAction();//return value "false" only when save
 
-int main(int argc, char *argv[]){
-    if(false){     //check if user have save file
-        if(false){ // chose = 0 , overrider save
-
-        }else{ // chose = 1 , load current profile
-
-        }
-
-    }
-    //test
-    printf("test main \n");
-    gen_map(1);
-
-    //generation map
-
-    //other action
-
-    // run game
+void main(int argc, char *argv[]){
+    int action;
     int gameProgress ;
-    gameProgress = false;
-    while(gameProgress){
-//        gameProgress = gameAction();
-    }
+    echo("Chose on action to do ");
+    echo("1 : load save");
+    echo("2 : New game");
+    echo("3 : test environnement");
+    scanf("%d",&action);
+    
+    switch (action) {
+        case 1:
+            if(false){     //check if user have save file
+                if(false){ // chose = 0 , overrider save
+                }else{ // chose = 1 , load current profile
+                }
+            }
+        case 2:
+            // run game
+            gameProgress = 0;
+            while(gameProgress){
+            // gameProgress = gameAction();
+            }
+            break;
+        case 3:
+            //test
 
-    return 0;
+            //generation map
+            printf("test main \n");
+            map *map;
+            map = gen_map(1);
+            printMap(map);
+
+            break;
+    }
 
 }
