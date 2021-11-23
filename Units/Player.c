6cfff7e707e087801af6aefd4d621f14d0240a42
CHWu <wuchin1997@gmail.com>
2021-11-23 20:15:29 +0100

    add player.c

---
+++ Units/Player.c
@@ -0,0 +1,22 @@
+//
+// Created by wuchi on 2021/11/18.
+//
+#include "Player.h"
+
+void playerMenu(Player player,int action){
+    //
+
+    switch (action) {
+        case 1:
+            move();
+
+
+
+    }
+
+}
+
+
+Player generationPlayer(int level , int currentExp , int currentHp){
+
+}
