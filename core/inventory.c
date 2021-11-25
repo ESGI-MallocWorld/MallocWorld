d9fe66100b099e6a6b7bf49d0f153e865f50cc9f
CHWu <wuchin1997@gmail.com>
2021-11-25 15:37:29 +0100

    restore fucntion
    restore function and add improve version ( wait to complet)

--- core/inventory.c
+++ core/inventory.c
@@ -124,8 +124,7 @@ void deleteElFromLinkedList(inventory* inv, Item* item){
         temp = inv; 
         inv = inv->next;
         free(temp);
-    }
-    else{
+    }else{
         while(inv->next != NULL){
             if(inv->next->inv->item->id == item->id){
                 temp = inv->next;
@@ -137,83 +136,109 @@ void deleteElFromLinkedList(inventory* inv, Item* item){
     }              
 }
 
-int deleteItemFromInv(Item* item, inventory* inv, int quantity){
+
+void deleteItemFromInv(Item* item, inventory* inv, int quantity){
     int stockInInv = getStockItem(item->id, inv);
-    int amount = quantity;
-    if(stockInInv > 0 && stockInInv < amount){ //if the inventory hasn't enough pieces of the looked for item
-        printf("You only have %d pieces of %s in the inventory \n", stockInInv, item->name);
-        return 0;
-    } 
-    else if(stockInInv == 0){ //if the item is not present in the inventory
-        printf("There is no %s present in the inventory\n", item->name);
-        return 0;
+    int newAmount = stockInInv - quantity;
+
+    inventory *inventoryData = getInventoryByItem(item , inv);
+
+    inventoryData->inv->stock = newAmount;
+
+    if(newAmount == 0 ){
+            temp = inv;
+            inv = inv->next;
+            free(temp);
     }
-    else{
-        *inventory temp;
-        int difference = amount - inv->inv->stock;
-        if(inv->inv->item->id == item->id){ // if item is stocked in the first case of the inventory
-            if(difference==0){//if the demanded quantity is equal to the stock present in the first item's case
-                temp = inv; 
+
+        if(stockInInv == 0){
+            printf("There is no %s present in the inventory\n", item->name);
+        }else if( stockInInv  < amount ){
+            printf("You only have %d pieces of %s in the inventory \n", stockInInv, item->name);
+        }else{
+            int difference = amount - inv->inv->stock;
+
+            do {
+                if(inv->inv->item->id == item->id){ // if item is stocked in the first case of the inventory
+                    if(difference==0){//if the demanded quantity is equal to the stock present in the first item's case
+                        temp = inv;
+                        inv = inv->next;
+                        free(temp);
+                        printf("%d piece(s) of %s were withdrawn from the inventory.\n", amount, item->name )
+                        return 1;
+                    }if else (difference>0){ //if the demanded amount is higher than the stock present in the first item's case
+                        amount -= inv->inv->stock;
+                        temp = inv;
+                        inv = inv->next;
+                        free(temp);
+
+                    }else{//if the stock in the inventory of the first item's case is higher than the demanded amount
+                        inv->inv->stock -= amount;
+                        printf("%d piece(s) of %s were withdrawn from the inventory.\n", amount, item->name )
+                        return 1;
+                    }
+                }
+
+                if(inv->next != NULL){
+                    inv = inv->next;
+                }
+            } while (inv->next != NULL);
+
+            //if item is not stocked in the first case or if the demanded amount is higher than what has already been withdrawn and the item is stocked in multiple cases
+            while(inv->next != NULL){
+                difference = amount - inv->next->inv->stock;
+                if(inv->next->inv->item->id == item->id){//checks if the item can be found in the next case
+                    if(difference==0){//if the demanded quantity is equal to the stock present in the next item's case
+                        temp = inv->next;
+                        inv->next = inv->next->next;
+                        free(temp);
+                        printf("%d piece(s) of %s were withdrawn from the inventory.\n", amount, item->name )
+                        return 1;
+                    }
+                    if else (difference>0){//if the demanded amount is higher than the stock present in the next item's case
+                        amount -= inv->next->inv->stock;
+                        temp = inv->next;
+                        inv->next = inv->next->next;
+                        free(temp);
+                    }
+                    else{//if the stock in the inventory of the next item's case is higher than the demanded amount
+                        inv->next->inv->stock -= amount;
+                        return 1;
+                        printf("%d piece(s) of %s were withdrawn from the inventory.\n", amount, item->name )
+                    }
+                }
                 inv = inv->next;
-                free(temp);
-                printf("%d piece(s) of %s were withdrawn from the inventory.\n", amount, item->name )
-                return 1;
             }
-            if else (difference>0){ //if the demanded amount is higher than the stock present in the first item's case
-                amount -= inv->inv->stock; 
-                temp = inv; 
-                inv = inv->next;
-                free(temp);
-                 
-            }
-            else{//if the stock in the inventory of the first item's case is higher than the demanded amount
-                inv->inv->stock -= amount;
-                printf("%d piece(s) of %s were withdrawn from the inventory.\n", amount, item->name )
-                return 1;
-            }       
+
         }
-        //if item is not stocked in the first case or if the demanded amount is higher than what has already been withdrawn and the item is stocked in multiple cases 
-        while(inv->next != NULL){
-            difference = amount - inv->next->inv->stock;
-            if(inv->next->inv->item->id == item->id){//checks if the item can be found in the next case
-                if(difference==0){//if the demanded quantity is equal to the stock present in the next item's case
-                    temp = inv->next;
-                    inv->next = inv->next->next;
-                    free(temp);
-                    printf("%d piece(s) of %s were withdrawn from the inventory.\n", amount, item->name )
-                    return 1;
-                }
-                if else (difference>0){//if the demanded amount is higher than the stock present in the next item's case
-                    amount -= inv->next->inv->stock; 
-                    temp = inv->next;
-                    inv->next = inv->next->next;
-                    free(temp);
-                }
-                else{//if the stock in the inventory of the next item's case is higher than the demanded amount
-                    inv->next->inv->stock -= amount;
-                    return 1;
-                    printf("%d piece(s) of %s were withdrawn from the inventory.\n", amount, item->name )
-                }
 
-                
-            }
-        inv = inv->next;
-        }  
-        
     }
 
+void deleteItemFromInv2(Item* item, inventory* inv, int quantity) {
+    int stockInInv = getStockItem(item->id, inv);
+    int newAmount = stockInInv - quantity;
+
+    inventory *inventoryData = getInventoryByItem(item, inv);
+
+    inventoryData->inv->stock = newAmount;
+
+    if (newAmount == 0) {
+        temp = inv;
+        inv = inv->next;
+        free(temp);
+    }
 }
 
 void moveItemFromInvPNJToInvPlayer(Item* item, int quantity, inventory* invPNJ, inventory* invPlayer){
-    deleteItemFromInv(item, invPNJ, quantity);
-    if (deleteItemFromInv == 1){
+    int res = deleteItemFromInv(item, invPNJ, quantity);
+    if (res == 1){ // créer une function qui controle les donner et puis appelle cette function
         addItemInvPlayer(item,invPlayer,quantity);
     }
 }
 
 void moveItemFromInvPlayerToInvPNJ(Item* item, int quantity, inventory* invPlayer, inventory* invPNJ){
-    deleteItemFromInv(item, invPlayer, quantity);
-    if (deleteItemFromInv == 1){
+    int res = deleteItemFromInv(item, invPlayer, quantity);
+    if (res == 1){
         addItemInvPNJ(item,invPNJ,quantity);
     }
 }
